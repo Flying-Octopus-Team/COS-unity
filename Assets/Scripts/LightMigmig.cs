ae7bb4f4b4c80a27f1eeb3d6374e7b8ead2160d8
Cosinus <oliwka_pawlowska@wp.pl>
2023-06-09 21:07:09 +0200

    Better lights + miging lights

--- Assets/Scripts/LightMigmig.cs
+++ Assets/Scripts/LightMigmig.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightMigmig : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class LightMigmig : MonoBehaviour {
+    public MeshRenderer meshRenderer;
+    public Material on;
+    public Material off;
+
+    void Start() {
         StartCoroutine(Migmig());
     }
 
@@ -16,8 +17,10 @@ public class LightMigmig : MonoBehaviour
             float randomTimeMigingOn = Random.Range(0.05f, 1f);
 
             GetComponent<Light>().enabled = false;
+            meshRenderer.material = off;
             yield return new WaitForSeconds(randomTimeMigingOff);
             GetComponent<Light>().enabled = true;
+            meshRenderer.material = on;
             yield return new WaitForSeconds(randomTimeMigingOn);
         }
     }
