dc51e1d30ec8f4c9804b53716559445501e2bc8c
karmeloova <olga.maruszynska@gmail.com>
2024-04-30 20:13:05 +0200

    Fire extinguisher end
    Added particles, better movement

--- Assets/FireExtinguisher/FireExtinguisherScript.cs
+++ Assets/FireExtinguisher/FireExtinguisherScript.cs
@@ -1,19 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using Random = UnityEngine.Random;
 
 public class FireExtinguisherScript : MonoBehaviour, IInteract
 { 
-    
-    private Rigidbody rb;
-    private float endTime = 0;
+
+    [Tooltip("Thrust force")][SerializeField] private float upForce = 0.1f;
+    [Tooltip("Side Thrust magnitude")][SerializeField] private float sideForce = 0.1f;
+    [Tooltip("Time up")][SerializeField] private float flyTime = 2.0f;
+
     private bool isInteract = false;
+    private float endTime = 0;
+    private Rigidbody rb;
+    private ParticleSystem feParticleSystem;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        feParticleSystem = GetComponent<ParticleSystem>();
+    }
     public void Interact()
     {
-        if(rb == null) return;
-        
+        if (rb == null) return;
         if (!isInteract)
         {
             StartCoroutine(Fly(2f));
@@ -22,23 +34,24 @@ public class FireExtinguisherScript : MonoBehaviour, IInteract
         }
     }
 
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
     IEnumerator Fly(float howLong)
     {
         endTime = Time.time + howLong;
+        rb.AddTorque(new Vector3(Random.Range(0.5f, 2f), Random.Range(0.5f, 2f), Random.Range(0.5f, 2f)));
         while (Time.time <= endTime)
         {
-            rb.AddForce(Vector3.up * 0.1f, ForceMode.Impulse);
+            rb.AddForce(this.transform.up * upForce + this.transform.right * MathF.Cos(Time.time*15)*sideForce, ForceMode.Impulse);
             yield return null;
+        }
+        feParticleSystem.Stop();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            feParticleSystem.Play();
+            isInteract = false;
         }
     }
 
