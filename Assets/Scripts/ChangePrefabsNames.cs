2324f44a115b377a5ff4150d597aa7e3ff9c0c2b
Cosinus215 <oliwka_pawlowska@wp.pl>
2024-05-11 21:00:29 +0300

    Fix naming

--- Assets/Scripts/ChangePrefabsNames.cs
+++ Assets/Scripts/ChangePrefabsNames.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-[ExecuteInEditMode]
 public class ChangePrefabsNames : MonoBehaviour {
-    public List<GameObject> prefabs = new List<GameObject>();
+    private List<GameObject> prefabs = new List<GameObject>();
 
-    [ContextMenu("LookForPrefabs")]
+    [ContextMenu("Look For Prefabs")]
     private void LookForPrefabs() {
         prefabs.Clear();
 
@@ -27,7 +25,7 @@ public class ChangePrefabsNames : MonoBehaviour {
 
     [ContextMenu("Rename instances of prefabs to its names")]
     void RenamePrefabs() {
-        for (int i = 0;i < prefabs.Count;i++) {
+        for (int i = 0; i < prefabs.Count; i++) {
             //Debug.Log(PrefabUtility.GetPrefabParent(prefabs[1]));
             Debug.Log(PrefabUtility.GetCorrespondingObjectFromOriginalSource(prefabs[i]));
             prefabs[i].name = PrefabUtility.GetCorrespondingObjectFromOriginalSource(prefabs[i]).name;
