7e0587d61bb9f74743c6febda423783ea228133e
Cosinus215 <oliwka_pawlowska@wp.pl>
2024-05-11 20:51:50 +0300

    Add script that changes name of prefabs on the scene to its original

---
+++ Assets/Scripts/ChangePrefabsNames.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[ExecuteInEditMode]
+public class ChangePrefabsNames : MonoBehaviour {
+    public List<GameObject> prefabs = new List<GameObject>();
+
+    [ContextMenu("LookForPrefabs")]
+    private void LookForPrefabs() {
+        prefabs.Clear();
+
+        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+        Debug.Log($"{allObjects.Count()} GameObjects found on the scene");
+
+        for (int i = 0; i < allObjects.Length; i++) {
+            if (PrefabUtility.GetPrefabInstanceStatus(allObjects[i]) == PrefabInstanceStatus.Connected) {
+                prefabs.Add(allObjects[i]);
+            }
+        }
+
+        Debug.Log($"{prefabs.Count()} Prefabs found on the scene");
+    }
+
+    [ContextMenu("Rename instances of prefabs to its names")]
+    void RenamePrefabs() {
+        for (int i = 0;i < prefabs.Count;i++) {
+            //Debug.Log(PrefabUtility.GetPrefabParent(prefabs[1]));
+            Debug.Log(PrefabUtility.GetCorrespondingObjectFromOriginalSource(prefabs[i]));
+            prefabs[i].name = PrefabUtility.GetCorrespondingObjectFromOriginalSource(prefabs[i]).name;
+        }
+    }
+
+}
