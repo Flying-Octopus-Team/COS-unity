e8eff3d5a2992ce6c0c7f65eddb251330d5e07ac
Cosinus215 <oliwka_pawlowska@wp.pl>
2024-05-13 21:12:12 +0300

    Add safe check on scene changed

--- Assets/Editor/ChangePrefabsNames.cs
+++ Assets/Editor/ChangePrefabsNames.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangePrefabsNames : Editor {
-    public static List<GameObject> prefabs = new List<GameObject>();
+    private static List<GameObject> prefabs = new List<GameObject>();
+    private static Scene activeScene;
 
     [MenuItem("Prefabs Settings/Look for prefabs")]
     public static void LookForPrefabs() {
         prefabs.Clear();
+        activeScene = SceneManager.GetActiveScene();
 
         GameObject[] allObjects = FindObjectsOfType<GameObject>();
         Debug.Log($"{allObjects.Count()} GameObjects found on the scene");
@@ -25,8 +28,17 @@ public class ChangePrefabsNames : Editor {
 
     [MenuItem("Prefabs Settings/Rename prefabs")]
     public static void RenamePrefabs() {
+        if (prefabs.Count == 0) {
+            Debug.LogError("No prefabs found on the scene! Make sure you run \"Look for prefabs\" before running \"Rename prefabs\"");
+            return;
+        }
+
+        if (activeScene != SceneManager.GetActiveScene()) {
+            prefabs.Clear();
+            RenamePrefabs();
+        }
+
         for (int i = 0; i < prefabs.Count; i++) {
-            //Debug.Log(PrefabUtility.GetPrefabParent(prefabs[1]));
             Debug.Log(PrefabUtility.GetCorrespondingObjectFromOriginalSource(prefabs[i]));
             prefabs[i].name = PrefabUtility.GetCorrespondingObjectFromOriginalSource(prefabs[i]).name;
         }
